f42283dca2801b37c565566827ea6b4268e8e668
TurramurraHighSchool <38341880+TurramurraHighSchool@users.noreply.github.com>
2018-06-12 06:54:26 +1000

    history

---
+++ save.js
@@ -0,0 +1,128 @@
+// JavaScript Document
+activityRoot="http://www.classtools.net/crossword/"
+silentsave=0;
+authorType="new";
+loadedData="";
+saveOrEdit="save";
+premiumUserName="NONE"
+function saveit(dataToSave)
+{
+numberOfQs=dataToSave.split("\n").length
+//Is it new (authorType=="new")?
+	//YES > 
+	if(authorType=="new")
+		{
+		//Ask for a new password.
+		apprise("<span align='center'>Provide a password<br>(so you can edit this later):</span>",
+			{'input':true}, 
+			function(pw) 
+				{
+				if(pw==false){apprise("A password must be provided");return}
+				correctPW=pw;
+									//Send to save.php.
+					$.ajax({ url: 'save.php',
+					 data: {
+							content:pw+"\n*****\n"+dataToSave,
+							theTitle:theTitle,
+							numberOfQs:numberOfQs
+							},
+					dataType: 'json',
+					type: 'post',
+					async:false,
+					success: function(data) 
+						{
+						apprise("Successfully saved.<br>Your template will now be loaded at its unique address.<br><b>Be sure to make a note of it!</b>",
+						{'animate':true},
+						function()
+							{
+							document.reloader.action=activityRoot+data.theURL;
+							document.reloader.pw.value=correctPW;
+							document.reloader.submit();
+							}
+							);
+						}
+					});
+				}
+		);
+		}
+
+	//NO > 
+		//	Are we dealing with an 'author viewer' already? (authorType=="administrator")
+		if(authorType=="administrator")
+			{
+			//YES > Resave and give a message.	
+				$.ajax({ url: 'save.php',
+				 data: {
+						content:correctPW+"\n*****\n"+dataToSave,
+						folder: theFolder,
+						file: theFile,
+						theTitle:theTitle,
+						numberOfQs:numberOfQs
+						},
+				dataType: 'json',
+				type: 'post',
+				async:false,
+				success: function(data) 
+					{
+					if(silentsave!=1)
+					//RESAVED to = http://www.classtools.net/brainybox/"+data.theURL
+						{
+						apprise("Successfully resaved",{'animate':true});
+						
+						}
+					silentsave=0;
+					hideEditScreen();
+					return true;
+					}
+				});
+			}	
+			//NO (i.e. authorType=="viewExisting") > 
+			if(authorType=="viewExisting")
+				{
+				//Ask for the password for this template.
+				apprise("The unlock the template at this URL, please provide the password:",
+						{'input':true}, 
+						function(pw) 
+							{
+							//CheckPW using Ajax.
+							checkpw(pw,"edit");	
+							}
+						);
+				}
+}
+
+function checkpw(pw,func)
+{
+$.ajax({ url: 'pwcheck.php',
+					 data: {
+							pw:pw,
+							folder: theFolder,
+							file: theFile
+							},
+					dataType: 'json',
+					type: 'post',
+					async:false,
+					success: function(data) 
+						{
+						//If incorrect, give error message and return
+						if(data.theMess=="failure")
+							{apprise("Incorrect password");return;}
+						else
+							{
+							correctPW=pw
+							authorType="administrator";
+							//If correct, give success message.
+							apprise("Correct!",{'animate':true},
+							function(r)
+								{
+								if(saveOrEdit=="save"){save();}
+								if(saveOrEdit=="edit")
+									{
+									editAllowed()
+									}
+								}
+							);	
+							}
+						}
+					});
+}
